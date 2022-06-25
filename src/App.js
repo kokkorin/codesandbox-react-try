20961560f995c14a8995819283ebe063aa9f1666
kokkorin <104617188+kokkorin@users.noreply.github.com>
2022-06-25 17:16:59 +0900

    カウンターコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-const CounterText = (props) => <p>カウント：{props.count}</p>;
+const CounterText = (props) => (
+  <p>
+    <b />
+    {props.name}のカウント：{props.count}
+  </p>
+);
 
 const INITIAL_COUNT = 0;
 
-const Counter = () => {
+const Counter = (props) => {
   const [count, setCount] = useState(INITIAL_COUNT);
 
-  const countAdd = () => setCount((prevCount) => prevCount + 1);
+  const countAdd = () => setCount((nowCount) => nowCount + 1);
 
-  const countSub = () => setCount((prevCount) => prevCount - 1);
+  const countSub = () => setCount((nowCount) => nowCount - 1);
 
   const countReset = () => setCount(INITIAL_COUNT);
 
   return (
     <>
+      <CounterText count={count} name={props.name} />
       <button onClick={countAdd}>ボタン +1</button>
       <button onClick={countSub}>ボタン -1</button>
       <button onClick={countReset}>リセット</button>
@@ -24,5 +30,11 @@ const Counter = () => {
 };
 
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <Counter name="腕立て伏せ" />
+      <Counter name="腹筋" />
+      <Counter name="スクワット" />
+    </>
+  );
 }
